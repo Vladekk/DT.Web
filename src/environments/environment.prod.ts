94bf65acfc9241003e5a345d9fc8e835eced06b5
vkugelevics <vladislavs.kugelevics@28stone.com>
2019-08-13 15:29:35 +0300

    fixed prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  ScheduleServiceUrl: 'https://upside.down.pictures/api/GetClosestRuns',
+  ScheduleServiceUrl: 'https://upside.down.pictures/api/',
   SchedulePageUrl: 'http://satiksme.daugavpils.lv/autobuss-nr-17a-autoosta-csdd-jaunforstadte',
   AllSchedulesUrl: 'http://www.satiksme.daugavpils.lv/autobusu-kustibu-saraksts'
 };
