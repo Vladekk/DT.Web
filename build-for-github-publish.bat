e024fe987235bae9e3f43005b77e846e40907370
vkugelevics <vladislavs.kugelevics@28stone.com>
2019-08-13 15:18:51 +0300

    publish

--- build-for-github-publish.bat
+++ build-for-github-publish.bat
@@ -1,4 +1,10 @@
 call npm version minor
+if errorlevel 1 goto Unsuccessful
 call ng build --prod --output-path docs
+if errorlevel 1 goto Unsuccessful
 copy ".\docs\index.html" ".\docs\404.html"
+if errorlevel 1 goto Unsuccessful
 echo upside.down.pictures > .\DOCS\CNAME
+if errorlevel 1 goto Unsuccessful
+
+:Unsuccessful
