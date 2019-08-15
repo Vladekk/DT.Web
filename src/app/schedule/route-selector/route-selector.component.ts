1885d4b4151c0c23d1b5a31d4a988308929d477a
vkugelevics <vladislavs.kugelevics@28stone.com>
2019-08-15 13:28:33 +0300

    Design changes Switched from css to scss

--- src/app/schedule/route-selector/route-selector.component.ts
+++ src/app/schedule/route-selector/route-selector.component.ts
@@ -1,16 +1,15 @@
 import {Component, OnInit} from '@angular/core';
-import {IRoute} from '../../IRoute';
-import {Route} from '../../Route';
+import {ActivatedRoute, ParamMap} from '@angular/router';
 
 import {ScheduleService} from '../../../services/schedule.service';
-import {ActivatedRoute, ParamMap} from '@angular/router';
+import {IRoute} from '../../IRoute';
+import {Route} from '../../Route';
 
 @Component({
   selector: 'app-route-selector',
   templateUrl: './route-selector.component.html',
   styleUrls: ['./route-selector.component.scss']
 })
-
 
 export class RouteSelectorComponent implements OnInit {
 
