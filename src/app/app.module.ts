198b90bd0010c22eb91e20a8162861b53ccbc87d
vkugelevics <vladislavs.kugelevics@28stone.com>
2019-08-20 15:27:31 +0300

    Cache fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {HttpClientModule} from '@angular/common/http';
-import {RouterModule, Routes} from '@angular/router';
-import {environment} from '../environments/environment.prod';
-import {ScheduleComponent} from './schedule/schedule.component';
 import {HashLocationStrategy, LocationStrategy} from '@angular/common';
-import {RouteSelectorComponent} from './schedule/route-selector/route-selector.component';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {NgxSpinnerModule} from 'ngx-spinner';
+import {environment} from '../environments/environment.prod';
+import {AppComponent} from './app.component';
 import {consoleLogServiceInstance, logServiceToken} from './logServiceToken';
+import {RouteSelectorComponent} from './schedule/route-selector/route-selector.component';
+import {ScheduleComponent} from './schedule/schedule.component';
 
 const appRoutes: Routes = [
-  //{path: 'crisis-center', component: CrisisListComponent},
   {path: 'Route/:routeNumber', component: ScheduleComponent},
   {path: '', redirectTo: 'Route/17A', pathMatch: 'full'},
   // {path: '**', component: PageNotFoundComponent}
