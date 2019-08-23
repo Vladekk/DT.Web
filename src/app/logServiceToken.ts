ac673c71910f688e8340adc74c197f5d1a4a491c
vkugelevics <vladislavs.kugelevics@28stone.com>
2019-08-23 11:41:02 +0300

    0.24.0

--- src/app/logServiceToken.ts
+++ src/app/logServiceToken.ts
@@ -1,11 +1,7 @@
 import {InjectionToken} from '@angular/core';
 import {ConsoleLogService} from './ConsoleLogService';
 
-// import {ISimpleLogService} from '../../../DT-Backend/src/services/SimpleLogService/ISimpleLogService';
-export interface ISimpleLogService {
-  Log(obj: any): void;
-}
-
+import {ISimpleLogService} from '../../../DT-Backend/src/services/SimpleLogService/ISimpleLogService';
 
 export let logServiceToken = new InjectionToken<ISimpleLogService>('');
 export let consoleLogServiceInstance = new ConsoleLogService();
